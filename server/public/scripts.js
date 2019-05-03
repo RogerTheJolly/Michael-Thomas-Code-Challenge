d2c53783f0f537662f23e2a12890ed915253c85a
RogerTheJolly <blobsrule72@yahoo.com>
2019-05-02 22:39:07 -0500

    Started response page, made quote page responsive
    Also added reset function on quote page

---
+++ server/public/scripts.js
@@ -0,0 +1,9 @@
+//Reset all input fields to default on the quote page
+function reset(){
+	document.getElementById('age').value = '';
+	document.getElementById('citizenship').value = '';
+	document.getElementById('state').value = '';
+	document.getElementById('startDate').value = null;
+	document.getElementById('endDate').value = null;
+	document.getElementById('maximum').selectedIndex = 0;
+}
